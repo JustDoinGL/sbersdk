2a4c11185c22819eacccc950229cf47ea202de3a
Andrew Chulkov <andrey.chulkov@list.ru>
2025-04-28 12:57:17 +0300

    Update sdk.tsx

--- sdk.tsx
+++ sdk.tsx
@@ -1,48 +1,10 @@
-import { Controller } from "react-hook-form";
-import { guidebookValidation } from "./guidebookValidation";
-
 const formatName = (value: string) => {
   return value
-    .split(" ")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(" ");
-};
-
-const FormField: FC<FormProps> = ({ element }) => {
-  const { control, formState: { errors } } = useFormContext();
-  const [isValid, setIsValid] = useState(false);
-
-  const handleChange = (value: string, onChange: (value: string) => void) => {
-    const formattedValue = formatName(value);
-    onChange(formattedValue); // Отправляем отформатированное значение в RHF
-    const validationResult = guidebookValidation[element.name].safeParse(formattedValue);
-    setIsValid(validationResult.success);
-  };
-
-  return (
-    <Controller
-      name={element.name}
-      control={control}
-      render={({ field }) => {
-        return (
-          <InputText
-            id={element.name}
-            name={element.name}
-            ref={field.ref}
-            onBlur={() => {
-              field.onBlur();
-              onBlurHandleImg(guidebookValidation[element.name], field.value, field.onBlur);
-            }}
-            onChange={(e) => handleChange(e.target.value, field.onChange)}
-            value={field.value}
-            size={inputSize.large}
-            label={element.label ?? ''}
-            placeholder={element.placeholder ?? ''}
-            error={errors[element.name]?.message ? `${errors[element.name]?.message}` : null}
-            valid={field.value && isValid}
-          />
-        );
-      }}
-    />
-  );
+    .split(/(\s|-)/) // Разбиваем по пробелам И дефисам
+    .map(part => 
+      part === " " || part === "-" 
+        ? part 
+        : part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
+    )
+    .join("");
 };
