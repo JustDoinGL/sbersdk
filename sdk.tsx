532c522c66474002d85a8c1f0ce75ce46268a0e0
Andrew Chulkov <andrey.chulkov@list.ru>
2025-05-20 22:17:31 +0300

    Update sdk.tsx

--- sdk.tsx
+++ sdk.tsx
@@ -1,37 +1,55 @@
-export const useQMath = (options: any, urls: any[]) => {
-    const [qrValues, setQrValues] = useState(urls.map(url => generatedUrl(url)));
+import { useEffect, useState } from 'react';
+import { generatedUrl } from '../constants/qr-code';
+import { Content } from '../constants/types';
+
+export const useQMath = (urls: Content[]) => {
+    const [qrValues, setQrValues] = useState(
+        urls.map(url => ({
+            qr: generatedUrl(url.qr),
+            button: generatedUrl(url.button)
+        }))
+    );
 
     useEffect(() => {
-        const intervals = urls.map((_, index) => {
+        const intervals = urls.map((url, index) => {
             let count = 0;
-            let oldUrl = new URL(qrValues[index]);
+            let oldQrUrl = new URL(qrValues[index].qr);
+            let oldButtonUrl = new URL(qrValues[index].button);
 
             const intervalId = setInterval(() => {
-                const newUrl = generateUrl(urls[index]);
-                const updatedUrl = generatedUrl(urls[index], { onlyCookiesParams: true });
+                const newQrUrl = generatedUrl(url.qr);
+                const newButtonUrl = generatedUrl(url.button);
+                
+                const updatedQrUrl = generatedUrl(url.qr, { onlyCookiesParams: true });
+                const updatedButtonUrl = generatedUrl(url.button, { onlyCookiesParams: true });
 
-                if (oldUrl.href !== updatedUrl.href) {
+                if (oldQrUrl.href !== updatedQrUrl.href || oldButtonUrl.href !== updatedButtonUrl.href) {
                     setQrValues(prev => {
                         const newValues = [...prev];
-                        newValues[index] = newUrl;
+                        newValues[index] = {
+                            qr: newQrUrl,
+                            button: newButtonUrl
+                        };
                         return newValues;
                     });
                 }
 
-                oldUrl = new URL(newUrl);
-                oldUrl.searchParams.delete('datetime');
+                oldQrUrl = new URL(newQrUrl);
+                oldButtonUrl = new URL(newButtonUrl);
+                oldQrUrl.searchParams.delete('detectme');
+                oldButtonUrl.searchParams.delete('detectme');
 
-                if (count >= options.iterations) {
+                if (count >= url.updateCookiesOptions?.iterations || 10) {
                     clearInterval(intervalId);
                 }
                 count += 1;
-            }, options.interval);
+            }, url.updateCookiesOptions?.interval || 1000);
 
             return intervalId;
         });
 
         return () => intervals.forEach(intervalId => clearInterval(intervalId));
-    }, [urls, options.interval, options.iterations]);
+    }, [urls]);
 
     return qrValues;
 };
