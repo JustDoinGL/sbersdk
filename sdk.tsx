abf544ac403c67318b7d199aaea0d440f0bdb922
Andrew Chulkov <andrey.chulkov@list.ru>
2025-06-14 23:09:59 +0300

    Update sdk.tsx

--- sdk.tsx
+++ sdk.tsx
@@ -1,4 +1,37 @@
-const pathSegments = window.location.pathname.split('/');
-if (pathSegments.includes('bank-partner')) {
-    console.log('Один из сегментов пути — "bank-partner"');
-}
+import { useCallback, useEffect, useRef, type RefObject } from "react";
+
+export const useSafariOrIphoneScrollToRef = () => {
+  const timeoutRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  const isIphone = () => /iPhone/i.test(navigator.userAgent);
+  const isSafari = () =>
+    /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+  const scrollToRef = useCallback((ref: RefObject<HTMLElement | null>) => {
+    if (!(isIphone() || isSafari())) return;
+
+    const scroll = () => {
+      ref.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    };
+
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(scroll, 0);
+  }, []);
+
+  return scrollToRef;
+};
+
