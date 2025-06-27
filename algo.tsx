f7091e42a29f8ba12186884a51ea845fa0113df6
Andrew Chulkov <andrey.chulkov@list.ru>
2025-06-27 16:09:21 +0300

    Update algo.tsx

--- algo.tsx
+++ algo.tsx
@@ -1,26 +1,38 @@
-module.exports = {
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      maxSize: 244 * 1024, // 244KB (меньше чем 250KB для лучшего кэширования)
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          priority: 10
+optimization: {
+  splitChunks: {
+    chunks: 'all',
+    maxSize: 244000, // ~244KB (меньше 256KB для лучшего кэширования)
+    cacheGroups: {
+      vendors: {
+        test: /[\\/]node_modules[\\/]/,
+        name: 'vendors',
+        priority: 10,
+        reuseExistingChunk: true
+      },
+      styles: {
+        test: /\.s?[ac]ss$/i,
+        name: 'styles',
+        chunks: 'all',
+        enforce: true,
+        priority: 20
+      },
+      // Новое правило для разбиения index.js
+      indexCommon: {
+        test: /[\\/]src[\\/]index\.js$|components[\\/]/,
+        name(module, chunks, cacheGroupKey) {
+          const moduleFileName = module.identifier().split('/').pop().split('.')[0];
+          return `index-${moduleFileName}`;
         },
-        common: {
-          name: 'common',
-          minChunks: 2, // Минимум 2 чанка используют модуль
-          priority: 5,
-          reuseExistingChunk: true
-        },
-        index: {
-          test: /[\\/]src[\\/]index\.js$/, // Или путь к вашему index.js
-          name: 'index',
-          priority: 20,
-          enforce: true
-        }
+        priority: 30,
+        minSize: 10000, // 10KB минимальный размер для выделения в отдельный чанк
+        minChunks: 1,
+        reuseExistingChunk: true
+      },
+      common: {
+        name: 'common',
+        minChunks: 2, // Минимум 2 чанка используют модуль
+        priority: 5,
+        reuseExistingChunk: true
       }
     }
   }
