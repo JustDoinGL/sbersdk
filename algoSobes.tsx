950733111d21c146326a20892f71f10618ac23ba
Andrew Chulkov <andrey.chulkov@list.ru>
2025-06-30 13:19:03 +0300

    Update algoSobes.tsx

--- algoSobes.tsx
+++ algoSobes.tsx
@@ -1,41 +1,44 @@
-/*
-Смерджить два массива с покупками. 
-Массивы приходят на вход всегда отсортированными по id. 
-У каждой покупки свой уникальной id (для бананов это 1, для апельсинов 2 и т.д.) .
-Нужно на выходе получить массив уникальных отсортированных по id покупок, с минимальной ценой.
-Сложность по времени не должна быть больше O(n)
-*/
-
-
-
 const merge = (arr1, arr2) => {
-  const mapa = []
-  const arr = [...purchases1, ...purchases2]
-
-  arr.forEach((el) => {
-    // if (mapa[el.id]) {
-      
-    //   if (mapa[el.id].price > el.price) {
-    //     mapa[el.id] = el
-    //   }
-    // } else {
-    //   mapa[el.id] = el
-    // }
-
-    if (mapa[el.id]) {
-      if ( mapa[el.id].price > el.price) {
-        mapa[el.id] = el
-      }
-      
-    } else {
-       mapa[el.id] = el
-    }
-  })
-
+  const result = [];
+  const map = {};
   
+  // Обрабатываем оба массива
+  let i = 0, j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i].id <= arr2[j].id) {
+      processElement(arr1[i], map, result);
+      i++;
+    } else {
+      processElement(arr2[j], map, result);
+      j++;
+    }
+  }
+  
+  // Обрабатываем оставшиеся элементы в arr1
+  while (i < arr1.length) {
+    processElement(arr1[i], map, result);
+    i++;
+  }
+  
+  // Обрабатываем оставшиеся элементы в arr2
+  while (j < arr2.length) {
+    processElement(arr2[j], map, result);
+    j++;
+  }
+  
+  return result;
+}
 
-    
-  return mapa.filter((el) => el)
+// Вспомогательная функция для обработки элемента
+function processElement(el, map, result) {
+  if (!map[el.id]) {
+    // Если элемент с таким id еще не встречался, добавляем его
+    map[el.id] = el;
+    result.push(el);
+  } else if (map[el.id].price > el.price) {
+    // Если нашли элемент с меньшей ценой, обновляем
+    map[el.id].price = el.price;
+  }
 }
 
 const purchases1 = [
@@ -62,16 +65,3 @@ const purchases2 = [
 ]
 
 console.log(merge(purchases1, purchases2))
-/*
-[
-    { id: 1, title: 'бананы', price: 100},
-    { id: 2, title: 'апельсины', price: 500},
-    { id: 3, title: 'мандарины', price: 100},
-    { id: 4, title: 'ананасы', price: 500},
-    { id: 5, title: 'яблоки', price: 500},
-    { id: 6, title: 'груши', price: 50},
-    { id: 7, title: 'черешня', price: 1000},
-    { id: 8, title: 'киви', price: 800},
-    { id: 9, title: 'манго', price: 900},
-]
-*/
