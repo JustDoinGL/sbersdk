57f940f80d59424f12ef3536e70132fd1aedab82
Andrew Chulkov <andrey.chulkov@list.ru>
2025-07-08 15:35:57 +0300

    Update sdk.tsx

--- sdk.tsx
+++ sdk.tsx
@@ -1,7 +1,30 @@
-<picture>
-  <!-- Это изображение будет показано на мобильных устройствах (ширина до 768px) -->
-  <source srcset="mobile-image.jpg" media="(max-width: 767px)">
-  
-  <!-- Это изображение будет показано на всех других устройствах -->
-  <img src="desktop-image.jpg" alt="Описание изображения">
-</picture>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: #3498db;
+  position: absolute;
+  animation: runAndFlip 4s infinite linear;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-weight: bold;
+}
+
+@keyframes runAndFlip {
+  0% {
+    right: 0;
+    transform: translateX(100%) rotateY(0deg);
+  }
+  50% {
+    right: 100%;
+    transform: translateX(-100%) rotateY(0deg);
+  }
+  50.1% {
+    transform: translateX(-100%) rotateY(180deg);
+  }
+  100% {
+    right: 0;
+    transform: translateX(100%) rotateY(180deg);
+  }
+}
