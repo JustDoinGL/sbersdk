f92c86e1f950c028f95d6faee11269ff644a8455
Andrew Chulkov <andrey.chulkov@list.ru>
2025-09-22 10:35:14 +0300

    Update algoO.js

--- algoO.js
+++ algoO.js
@@ -1,19 +1,8 @@
-type TransformArrays<T> = T extends Array<infer U>
-  ? string
-  : T extends object
-    ? { [K in keyof T]: TransformArrays<T[K]> }
-    : T;
-
-function mapArraysToStringsReduce<T extends Record<string, unknown>>(
-  obj: T
-): TransformArrays<T> {
+function mapFlatArraysToStrings<T extends object>(obj: T): FlatArrayToStringMapper<T> {
   return Object.entries(obj).reduce((acc, [key, value]) => {
-    if (Array.isArray(value)) {
-      return { ...acc, [key]: value.join(', ') };
-    } else if (value !== null && typeof value === 'object' && !Array.isArray(value)) {
-      return { ...acc, [key]: mapArraysToStringsReduce(value as Record<string, unknown>) };
-    } else {
-      return { ...acc, [key]: value };
-    }
-  }, {} as Record<string, unknown>) as TransformArrays<T>;
+    return {
+      ...acc,
+      [key]: Array.isArray(value) ? value.join(', ') : value
+    };
+  }, {} as Record<string, unknown>) as FlatArrayToStringMapper<T>;
 }
