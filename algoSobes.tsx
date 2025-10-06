d4225236ca8f0f9917bf13ac4e83d54af02a1380
Andrew Chulkov <andrey.chulkov@list.ru>
2025-10-06 12:20:34 +0300

    Update algoSobes.tsx

--- algoSobes.tsx
+++ algoSobes.tsx
@@ -1,31 +1,23 @@
-// Добавьте этот хук в компонент формы или создайте отдельный компонент-синхронизатор
-const useSyncPhoneFields = () => {
-    const { control, setValue } = useFormContext();
+
+import { useEffect, useContext } from "react";
+import { UNSAFE_NavigationContext } from "react-router-dom";
+
+const useBackListener = (callback) => {
+  const navigator = useContext(UNSAFE_NavigationContext).navigator;
+
+  useEffect(() => {
+    // Создаем слушателя, который получает событие навигации
+    const listener = ({ location, action }) => {
+      // action может быть "PUSH", "POP" или "REPLACE"
+      if (action === "POP") {
+        callback({ location, action });
+      }
+    };
     
-    const phone1 = useWatch({ control, name: 'PhoneNumber' });
-    const phone2 = useWatch({ control, name: 'PhoneNumber2' });
-
-    React.useEffect(() => {
-        if (phone1 && phone1 !== phone2) {
-            setValue('PhoneNumber2', phone1, { 
-                shouldValidate: true,
-                shouldDirty: true 
-            });
-        }
-    }, [phone1]);
-
-    React.useEffect(() => {
-        if (phone2 && phone2 !== phone1) {
-            setValue('PhoneNumber', phone2, { 
-                shouldValidate: true,
-                shouldDirty: true 
-            });
-        }
-    }, [phone2]);
-};
-
-// Используйте в вашем основном компоненте формы
-const YourFormComponent = () => {
-    useSyncPhoneFields();
-    // ... остальной код формы
+    // Подписываемся на события навигации
+    const unlisten = navigator.listen(listener);
+    
+    // Отписываемся от событий при размонтировании компонента
+    return unlisten;
+  }, [callback, navigator]);
 };
