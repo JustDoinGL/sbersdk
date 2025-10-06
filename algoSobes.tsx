02b00b45a4ed2dc40d1a881424d4d3a63cb52b1d
Andrew Chulkov <andrey.chulkov@list.ru>
2025-10-06 12:22:22 +0300

    Update algoSobes.tsx

--- algoSobes.tsx
+++ algoSobes.tsx
@@ -1,23 +1,17 @@
+import { useNavigationType } from "react-router-dom";
+import { useEffect } from "react";
 
-import { useEffect, useContext } from "react";
-import { UNSAFE_NavigationContext } from "react-router-dom";
-
-const useBackListener = (callback) => {
-  const navigator = useContext(UNSAFE_NavigationContext).navigator;
-
+function MyComponent() {
+  const navigationType = useNavigationType();
+  
   useEffect(() => {
-    // Создаем слушателя, который получает событие навигации
-    const listener = ({ location, action }) => {
-      // action может быть "PUSH", "POP" или "REPLACE"
-      if (action === "POP") {
-        callback({ location, action });
-      }
-    };
-    
-    // Подписываемся на события навигации
-    const unlisten = navigator.listen(listener);
-    
-    // Отписываемся от событий при размонтировании компонента
-    return unlisten;
-  }, [callback, navigator]);
-};
+    // Сработает при любой навигации, но только для типа POP (назад/вперед)
+    if (navigationType === "POP") {
+      console.log("Сработала навигация Назад/Вперед");
+      // Вызов вашей функции
+      myCustomFunction();
+    }
+  }, [navigationType]);
+  
+  return <div>Содержимое компонента</div>;
+}
