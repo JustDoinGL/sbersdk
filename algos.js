66f1389572b9707b14456202c3c2efe2bdc3aa70
Andrew Chulkov <andrey.chulkov@list.ru>
2025-10-06 18:39:06 +0300

    Update algos.js

--- algos.js
+++ algos.js
@@ -1,48 +1,40 @@
-import { useNavigationType, useLocation } from "react-router-dom";
-import { useEffect, useRef } from "react";
+const onSubmit = (data: FormCrmData) => {
+  // Отправляем событие в аналитику при переходе к экрану кросс-сейла
+  dataLayerPush({
+    action: 'go_to_cross_sell_from_fos',
+    label: analyticLabel
+  });
 
-export const useBackForwardListener = (callback: () => void) => {
-  const navigationType = useNavigationType();
-  const location = useLocation();
-  const prevLocationKey = useRef(location.key);
-
-  useEffect(() => {
-    // Проверяем, что это навигация по истории и location изменился
-    if (navigationType === "POP" && prevLocationKey.current !== location.key) {
-      console.log("Сработала навигация Назад/Вперед", {
-        previousKey: prevLocationKey.current,
-        currentKey: location.key,
-        pathname: location.pathname
-      });
-      
-      // Вызываем переданную функцию
-      callback();
-      
-      // Обновляем предыдущий ключ
-      prevLocationKey.current = location.key;
+  // Перезаписываем leadAdditionalParameters
+  const newForm = {
+    ...form,
+    [service]: {
+      ...form[service],
+      leads: {
+        ...form[service].leads,
+        data_leadAdditionalParameters: data.leadAdditionalParameters // перезаписываем
+      }
     }
-  }, [navigationType, location, callback]);
-};
+  };
 
-// Альтернативная версия с дополнительной информацией
-export const useBackForwardListenerAdvanced = (callback: (location: any) => void) => {
-  const navigationType = useNavigationType();
-  const location = useLocation();
-  const prevLocationKey = useRef(location.key);
-
-  useEffect(() => {
-    if (navigationType === "POP" && prevLocationKey.current !== location.key) {
-      console.log("Навигация по истории:", {
-        type: navigationType,
-        from: prevLocationKey.current,
-        to: location.key,
-        fullLocation: location
-      });
-      
-      // Передаем location в callback для большей гибкости
-      callback(location);
-      
-      prevLocationKey.current = location.key;
+  // Формируем данные для отправки запроса
+  setFormData({
+    url: form.service.url,
+    data: {
+      ...sberCrmDataCollection({
+        ...newForm // используем обновленную форму
+      }, data)
     }
-  }, [navigationType, location, callback]);
+  });
+
+  if (timerId) {
+    dispatch(
+      submitRequest({
+        analyticLabel,
+        keepalive: false,
+        visitorId: visitorId.current,
+        productName: null
+      })
+    );
+  }
 };
