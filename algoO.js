ed2fa3dd6ca7c596fb0e116172bbeb7728e2cb89
Andrew Chulkov <andrey.chulkov@list.ru>
2025-10-17 12:27:57 +0300

    Update algoO.js

--- algoO.js
+++ algoO.js
@@ -1,19 +1,20 @@
+import { z } from "zod";
+
 export const smsCodeSchema = z.object({
   digits: z.array(
-    z.number({ 
+    z.number({
       required_error: "Все поля должны быть заполнены",
-      invalid_type_error: "Должна быть цифра"
-    }).min(0).max(9)
+      invalid_type_error: "Должна быть цифра",
+    })
+    .min(0)
+    .max(9)
   ).length(6, "Код должен содержать 6 цифр")
-}).refine(
-  (data) => {
-    const fullCode = data.digits.join('');
-    return /^\d{6}$/.test(fullCode);
-  },
-  {
-    message: "Код должен содержать ровно 6 цифр",
-    path: ["digits"]
-  }
+}).pipe(
+  z.object({
+    digits: z.array(z.number())
+  }).transform((data) => ({
+    code: data.digits.join("")
+  }))
 );
 
 export type SmsCodeSchema = z.infer<typeof smsCodeSchema>;
