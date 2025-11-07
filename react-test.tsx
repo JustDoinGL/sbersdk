dd398d529f5a7929c3ee2730fcd03eedd4366f54
Andrew Chulkov <andrey.chulkov@list.ru>
2025-11-07 11:04:48 +0300

    Update react-test.tsx

--- react-test.tsx
+++ react-test.tsx
@@ -1,25 +1,21 @@
-export const mapDtoToSchema = (filters: Filters): ActivityFiltersFormSchema => {
-    return {
-        client: filters.client,
-        activity_type: filters.activity_types?.split(",").map(id => {
-            const found = activityType.find(type => type.id.toString() === id.trim());
-            return { label: found!.name, value: found!.id.toString() };
-        }),
-        activity_status: filters.activity_statuses?.split(",").map(id => {
-            const found = activityStatus.find(status => status.id.toString() === id.trim());
-            return { label: found!.name, value: found!.id.toString() };
-        }),
-        deadline: filters.deadline ? [new Date(filters.deadline)] : undefined,
-    };
-};
-
-export const mapSchemaToDto = (
-    schema: ActivityFiltersFormSchema,
-): Record<string, string | string[] | undefined> => {
-    return {
-        client: schema.client,
-        activity_types: schema.activity_type?.map(item => item.value).join(","),
-        activity_statuses: schema.activity_status?.map(item => item.value).join(","),
-        deadline: schema.deadline?.[0]?.toISOString(),
-    };
+export const mapSchemaToDto = (schema: ActivityFiltersFormSchema): Filters => {
+    const data: Record<string, string> = {};
+    
+    if (schema.client) {
+        data.client = schema.client;
+    }
+    
+    if (schema.activity_type?.length) {
+        data.activity_types = schema.activity_type.map(item => item.value).join(",");
+    }
+    
+    if (schema.activity_status?.length) {
+        data.activity_statuses = schema.activity_status.map(item => item.value).join(",");
+    }
+    
+    if (schema.deadline?.length) {
+        data.deadline = schema.deadline.map(date => date.getTime().toString()).join("%2C");
+    }
+    
+    return data;
 };
