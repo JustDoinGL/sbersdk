b9b79788b005eebd90e5c6476829569754a66649
Andrew Chulkov <andrey.chulkov@list.ru>
2025-11-07 10:27:34 +0300

    Update react-test.tsx

--- react-test.tsx
+++ react-test.tsx
@@ -1,31 +1,14 @@
-import { ActivityFiltersFormSchema } from "./schema";
-import { referencesService } from "@/5_shared/reference";
-import { Filters } from "@/5_shared/ui";
-
-const activityStatus = referencesService.getDictSet("activity_status");
-const activityType = referencesService.getDictSet("activity_type");
-
-export const activityTypesOptions = activityType.map((type) => ({ 
-    label: type.name, 
-    value: type.id.toString(),
-}));
-
-export const activityStatusOptions = activityStatus.map((type) => ({ 
-    label: type.name, 
-    value: type.id.toString(),
-}));
-
 export const mapDtoToSchema = (filters: Filters): ActivityFiltersFormSchema => {
     return {
         client: filters.client,
-        activity_types: filters.activity_types?.split(",").map(id => {
+        activity_type: filters.activity_types?.split(",").map(id => {
             const found = activityType.find(type => type.id.toString() === id.trim());
-            return found ? { label: found.name, value: found.id.toString() } : null;
-        }).filter(Boolean),
-        activity_statuses: filters.activity_statuses?.split(",").map(id => {
+            return { label: found!.name, value: found!.id.toString() };
+        }),
+        activity_status: filters.activity_statuses?.split(",").map(id => {
             const found = activityStatus.find(status => status.id.toString() === id.trim());
-            return found ? { label: found.name, value: found.id.toString() } : null;
-        }).filter(Boolean),
+            return { label: found!.name, value: found!.id.toString() };
+        }),
         deadline: filters.deadline ? [new Date(filters.deadline)] : undefined,
     };
 };
@@ -35,8 +18,8 @@ export const mapSchemaToDto = (
 ): Record<string, string | string[] | undefined> => {
     return {
         client: schema.client,
-        activity_types: schema.activity_types?.map(item => item.value).join(","),
-        activity_statuses: schema.activity_statuses?.map(item => item.value).join(","),
+        activity_types: schema.activity_type?.map(item => item.value).join(","),
+        activity_statuses: schema.activity_status?.map(item => item.value).join(","),
         deadline: schema.deadline?.[0]?.toISOString(),
     };
 };
