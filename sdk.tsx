b419ada1e2b9b37ea57b201c746cbbb1b6b318f9
Andrew Chulkov <andrey.chulkov@list.ru>
2025-11-24 14:24:51 +0300

    Update sdk.tsx

--- sdk.tsx
+++ sdk.tsx
@@ -1,1 +1,7 @@
-"Введите номер телефона для получения SMS для подтверждения актов"
+function formatPhone(phone) {
+    return phone.replace(/(\+\d)(\d{3})(\d{3})(\d{2})(\d{2})/, '$1 ($2) $3-$4-$5');
+}
+
+// Пример использования:
+const result = formatPhone("+73456787654");
+console.log(result); // "+7 (345) 678-76-54"
