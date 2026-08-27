e386ff210418c3cb938d2722634e167d2cdb6984
ㅤ <andrey.chulkov@list.ru>
2026-08-27 11:41:52 +0300

    Update sdk.tsx

--- sdk.tsx
+++ sdk.tsx
@@ -1,79 +1,38 @@
-type PaymentFrequency =
-  | 'Single'
-  | 'TwoEqualPayments'
-  | 'Quarterly'
-  | 'Other';
+const AccidentContent = () => {
+  // ...
 
-type BaseRateAttributes = {
-  coveragePeriod?: string;
-  payoutOption?: string;
-};
+  const createDraft = async () => {
+    await form.handleSubmit(
+      async (data: OutputOsagoSchema) => {
+        const result = await createDraftAction(data);
 
-type CoefficientAttributes = {
-  insuredQuantity: number;
-  professionKind: string;
-  ruleNumber?: number;
-  professionId?: string;
-  sumInsuredOrder: string;
-  duration: number;
-  payPercent?: number;
-  payDays?: number;
-  payOutTable?: string;
-  antimite?: boolean;
-  toxicoInfectionFood?: boolean;
-  improperMedicalManipulations?: boolean;
-  sport?: boolean;
-  sportTypesIds?: string[];
-  lossFreeYears?: number;
-  active?: boolean;
-  specialProgram?: string;
-  promo?: string;
-};
+        await applyCalculationResult({
+          ...result,
+          successTitle: 'Черновик успешно создан',
+        });
+      },
+      (errors) => {
+        console.error({ errors });
 
-type InsuranceRiskAttributes = {
-  baseRateAttributes: BaseRateAttributes;
-  coefficientAttributes: CoefficientAttributes;
-};
+        const msg =
+          'На одном из экранов есть неправильно заполненные поля';
 
-type InsuredObject = {
-  id: string;
-  code: string;
-  type: string;
-  description: string;
-  sumInsured: number;
-  groupNames: string[];
-};
+        form.setError('root', {
+          message: msg,
+        });
+      },
+    )();
+  };
 
-type InsuranceRisk = {
-  id: string;
-  objectRef: string;
-  sumInsured?: number;
-  boxedProgramDuration?: number;
-  insuranceVariant: string;
-  massEventVariantNumber?: number;
-  elementaryRisk?: string;
-  programId: string;
-  elementaryRiskId: string;
-  attributes?: InsuranceRiskAttributes;
-};
+  const Component = accidentSteps[currentStep].screen;
 
-type PaymentPlanItem = {
-  paymentDate: string;
-  paymentAmount: number;
-  paymentNumber: number;
-};
-
-type PaymentData = {
-  paymentFrequency: PaymentFrequency;
-  paymentPlan?: PaymentPlanItem[];
-};
-
-type CalculationRequest = {
-  objects: InsuredObject[];
-  items: InsuranceRisk[];
-  paymentData?: PaymentData;
-  startDate: string;
-  endDate: string;
-  sumInsuredMethod: string;
-  sumInsuredOrder: string;
+  return (
+    <ProductContextProvider
+      handleCreateDraft={createDraft}
+      handleAppointmentCalculation={() => {}}
+      handleDownloadDocument={() => {}}
+    >
+      <Component />
+    </ProductContextProvider>
+  );
 };
